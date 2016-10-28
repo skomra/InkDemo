5b9c9c2168973c4f9e2fc897d5e46bac78e695a4
Aaron Armstrong Skomra <skomra@gmail.com>
2016-10-28 10:01:03 -0700

    add CreateActivity for making and editing notes

---
+++ app/src/main/java/com/wacom/skomra/inkdemo/CreateActivityFragment.java
@@ -0,0 +1,22 @@
+package com.wacom.skomra.inkdemo;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class CreateActivityFragment extends Fragment {
+
+    public CreateActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_create, container, false);
+    }
+}
